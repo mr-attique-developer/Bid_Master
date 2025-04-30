56a536a568d426b1058581354d3a87071703973f
Muhammad Attique <mr.unknown5264@gmail.com>
2025-04-30 23:02:30 +0500

    Implement user authentication and profile management features
    - Added JWT token generation and cookie handling for user registration and login.
    - Enhanced user registration with password confirmation and bio fields in the user model.
    - Updated user routes to include middleware protection for sensitive routes.
    - Implemented user profile fetching, updating, and account deletion functionalities.

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,12 @@
 import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 import sendEmail from "../utils/email.js"
+import jwt from "jsonwebtoken"
 
+
+const generateToken = (id)=>{
+return jwt.sign({id}, process.env.JWT_SECRET,{expiresIn: process.env.JWT_EXPIRES_IN})
+}
 
 export const registerUser1 = async(req, res) =>{
     try {
@@ -55,10 +60,18 @@ export const registerUser1 = async(req, res) =>{
             fullName,
             password: hashedPassword,
         })
+        const token=  generateToken(newUser._id)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production", // only secure in production
+            sameSite: "strict",
+            maxAge: 30 * 24 * 60 * 60 * 1000,
+          }); 
         res.status(201).json({
             success: true,
             message: "User registered successfully",    
             user: newUser,
+            token,
             isActive:false
         })
 
@@ -75,8 +88,9 @@ export const registerUser1 = async(req, res) =>{
 
 export const registerUser2 = async(req,res)=>{
 try {
-    const {userId, businessName, businessDescription, location, role} = req.body
-    const phone = req.body.phone.replace(/\D/g, '');
+    const userId = req.user._id
+    console.log(userId)
+    const { businessName, businessDescription, location, role,phone} = req.body
     if(!phone || !businessName || !businessDescription || !location || !role){
         return res.status(400).json({
             success: false,
@@ -84,28 +98,24 @@ try {
         })
         
     }
-    const phoneRegex = /^0[3][0-9]{9}$/;
-    if (!phoneRegex.test(phone)) {
+    const cleanPhoneDigit =  phone.replace(/\D/g, '');
+    const phoneRegex = /^(0[3][0-9]{9}|92[3][0-9]{9})$/;
+    if (!phoneRegex.test(cleanPhoneDigit)) {
         return res.status(400).json({
             success: false,
-            message: "Please enter a valid 11-digit Pakistani phone number (e.g., 0300 1234567)",
+            message: "Invalid Pakistani number. Use 0300 1234567 or 92 300 1234567",
         });
     }
-if(phone.length < 11 || phone.length > 11){
-    return res.status(400).json({
-        success: false,
-        message: "Phone number must be 11 digits long",
-    })
-}
-
+    const formatedPhone = cleanPhoneDigit.startsWith("92") ? "0" + cleanPhoneDigit.substring(2) : cleanPhoneDigit
 if(role !== "buyer" && role !== "seller" && role !== "admin"){
     return res.status(400).json({
         success: false,
         message: "Role must be either buyer, seller or admin",
     })
 }
+
 const user = await User.findByIdAndUpdate(userId, {
-    phone,
+    phone : formatedPhone,
     businessName,
     businessDescription,
     location,
@@ -134,4 +144,279 @@ res.status(200).json({
 } catch (error) {
     console.log(error.message)
 }
+}
+
+
+
+export const loginUser = async(req,res)=>{
+    try {
+        const {email, password} = req.body
+        if(!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all the fields",
+            })
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Please enter a valid email address",
+            })
+        }
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({
+                success: false,
+                message: "Invalid credentials",
+            })
+        }
+        const token = generateToken(user._id)
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production", // only secure in production
+            sameSite: "strict",
+            maxAge: 30 * 24 * 60 * 60 * 1000,
+          });
+        res.status(200).json({
+            success: true,
+            message: "User logged in successfully",
+            token,
+            user:{
+                id:user._id,
+                email:user.email,
+                fullName:user.fullName,
+                phone:user.phone,
+                businessName:user.businessName,
+                businessDescription:user.businessDescription,
+                location:user.location,
+                role:user.role,
+                isActive:user.isActive
+            }
+        })
+    } catch (error) {
+        console.log("The Error in login controller ",error.message)
+        res.status(500).json({
+            success: false,
+            message: "Error in logging in user",
+        })
+        
+    }
+}
+
+export const logoutUser = async(req,res)=>{
+    try {
+        
+        res.cookie("token", null, {
+            expires: new Date(Date.now()),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            sameSite: "strict"
+        })
+        res.status(200).json({
+            success: true,
+            message: "User logged out successfully",
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            success: false,
+            message: "Error in logging out user",
+        })
+        
+    }
+}
+
+export const getUserProfile = async(req,res) =>{
+    try {
+        const userId  = req.user._id
+        
+        // console.log(userId)
+        const user = await User.findById(userId).select("-password")
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "User profile fetched successfully",
+            user,
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            success: false,
+            message: "Error in getting user profile",
+        })
+        
+    }
+}
+
+export const updateUserProfile = async(req,res) =>{
+    try {
+        const userId = req.user._id
+        const {email, fullName, phone, businessName, businessDescription, location, bio, role} = req.body
+        if(!email || !fullName || !phone || !businessName || !businessDescription || !location || !bio || !role){
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all the fields",
+            })
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({
+                success: false,
+                message: "Please enter a valid email address",
+            })
+        }
+        const phoneRegex = /^0[3][0-9]{9}|92[3][0-9]{9}$/;
+        const cleanPhoneDigit =  phone.replace(/\D/g, '');
+        if( !phoneRegex.test(cleanPhoneDigit)) {
+            return res.status(400).json({
+                success: false,
+                message: "Please enter a valid 11-digit Pakistani phone number (e.g., 0300 1234567)",
+            });
+        }
+        const formatedPhone = cleanPhoneDigit.startsWith("92") ? "0" + cleanPhoneDigit.substring(2) : cleanPhoneDigit
+    
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            email,
+            fullName,
+            phone : formatedPhone,
+            businessName,
+            businessDescription,
+            location,
+            bio,
+            role
+        }, {new: true, select: "-password"})
+        if(!updatedUser){
+            return res.status(400).json({
+                success: false,
+                message: "Error in updating user profile",
+            })
+        }
+        res.status(200).json({
+            success: true,
+            message: "User profile updated successfully",
+            user: updatedUser,
+        })
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            success: false,
+            message: "Error in updating user profile",
+        })
+        
+    }
+}
+
+export const updatePassword = async(req,res)=>{
+    try {
+        const userId = req.user._id
+        const {oldPassword, newPassword, confirmPassword} = req.body
+        if(!oldPassword || !newPassword || !confirmPassword){
+            return res.status(400).json({
+                success: false,
+                message: "Please fill all the fields",
+            })
+        }
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        if(!passwordRegex.test(newPassword)){
+            return res.status(400).json({
+                success: false,
+                message: "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one number",
+            })
+        }
+        if(newPassword !== confirmPassword){
+            return res.status(400).json({
+                success: false,
+                message: "New password and confirm password do not match",
+            })
+        }
+        const user = await User.findById(userId)
+        if(!user){
+            return res.status(400).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+        const isMatch = await bcrypt.compare(oldPassword, user.password)
+        if(!isMatch){
+            return res.status(400).json({
+                success: false,
+                message: "Old password is incorrect",
+            })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(newPassword, salt)
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            password: hashedPassword,
+            passwordChangedAt: Date.now()
+        }, {new: true, select: "-password"})   
+        if(!updatedUser){
+            return res.status(400).json({
+                success: false,
+                message: "Error in updating password",
+            })
+        }
+        const token = generateToken(updatedUser._id)
+        res.status(200).json({
+            success: true,
+            message: "Password updated successfully",
+            user: updatedUser,
+            token
+        }) 
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            success: false,
+            message: "Error in updating password",
+        })
+        
+    }
+}
+
+
+export const deleteAccount = async(req,res)=>{
+    try {
+        const userId = req.user._id
+        console.log(userId)
+        const deletedUser = await User.findByIdAndDelete(userId)
+        if(!deletedUser){
+            res.status(400).json({
+                success: false,
+                message: "User Not Found",
+        })
+    }
+    res.clearCookie("token")
+    res.status(200).json({
+        sucess: true,
+        message:"User Deleted Successfully"
+    })
+
+        
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({
+            success: false,
+            message: "Error in deleting account",
+        })
+        
+    }
 }
